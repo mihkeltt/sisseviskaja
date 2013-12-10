28b131da4acc28e73785e2444dd8748557b4165c
mare hunt <mare.hunt@netgroup.ee>
2013-12-10 15:10:12 +0200

    initial commit

---
+++ assets/scripts/main.js
@@ -0,0 +1,324 @@
+// JavaScript Document
+
+var desktopVolume = {
+    0: '<span class="violet-text" style="line-height: 2.5;">Ei soovi</span>',
+    1: '<span class="violet-text">1M / 0,5M</span><br>alla / üles',
+    2: '<span class="violet-text">10M / 5M</span><br>alla / üles',
+    3: '<span class="violet-text">20M / 5M</span><br>alla / üles',
+    4: '<span class="violet-text">30M / 10M</span><br>alla / üles',
+    5: '<span class="violet-text">50M / 30M</span><br>alla / üles'
+};
+
+var desktopPrice = {
+    0: "0",
+    1: "25",
+    2: "32",
+    3: "45",
+    4: "135",
+    5: "300"
+};
+
+var phoneVolume = {
+    0: '<span class="violet-text">Ei soovi</span>',
+    1: '<span class="violet-text">60</span> minutit',
+    2: '<span class="violet-text">120</span> minutit',
+    3: '<span class="violet-text">240</span> minutit',
+};
+
+var phonePrice = {
+    0: "0",
+    1: "1.66",
+    2: "2.99",
+    3: "5.66",
+};
+
+var mobileVolume = {
+    0: '<br /><span class="violet-text">Ei soovi</span>',
+    1: '<span class="violet-text">1,5GB</span><br>3M alla /<br>1M üles',
+    2: '<span class="violet-text">5GB</span><br>21,6M alla /<br>5,7M üles',
+    3: '<span class="violet-text">10GB</span><br>50M alla /<br>5M üles',
+    4: '<span class="violet-text">20GB</span><br>100M alla /<br>50M üles',
+    5: '<span class="violet-text">25GB</span><br>100M alla /<br>50M üles',
+};
+
+var mobilePrice = {
+    0: "0",
+    1: "6.49",
+    2: "9.95",
+    3: "16.96",
+    4: "29.95",
+    5: "35.95"
+};
+
+var mobileAddedMin = {
+    0: "0",
+    1: "0",
+    2: "0",
+    3: "150",
+    4: "300",
+    5: "500"
+};
+
+var mobileAddedSms = {
+    0: "0",
+    1: "0",
+    2: "0",
+    3: "50",
+    4: "100",
+    5: "200"
+};
+
+var mobileVolumeVol = {
+
+    0: '<span class="violet-text" id="minsum"></span> minutit<br><span class="violet-text" id="smssum"></span> sms-i',
+    1: '<span class="violet-text" id="minsum"></span> minutit<br><span class="violet-text" id="smssum"></span> sms-i',
+    2: '<span class="violet-text" id="minsum"></span> minutit<br><span class="violet-text" id="smssum"></span> sms-i',
+};
+
+var mobileVolumeVolSms = {
+
+    0: "40",
+    1: "90",
+    2: "200"
+}
+
+var mobileVolumeVolMin = {
+
+    0: "400",
+    1: "900",
+    2: "2000"
+}
+
+var mobilePriceVol = {
+
+    0: "6",
+    1: "12",
+    2: "24",
+};
+
+var dongleVolume = {
+    0: '<span class="violet-text">15GB</span><br>2,5M alla /<br>384K üles',
+    1: '<span class="violet-text">15GB</span><br>5M alla /<br>1M üles',
+    2: '<span class="violet-text">30GB</span><br>15M alla /<br>3M üles',
+    3: '<span class="violet-text">30GB</span><br>50M alla /<br>25M üles',
+    4: '<span class="violet-text">60GB</span><br>150M alla /<br>50M üles'
+};
+
+var donglePrice = {
+    0: "7.45",
+    1: "11.95",
+    2: "15.95",
+    3: "24.95",
+    4: "50.00"
+};
+
+function ReplaceNumberWithCommas(yourNumber) {
+    //Seperates the components of the number
+    var n= yourNumber.toString().split(".");
+    //Comma-fies the first part
+    n[0] = n[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    //Combines the two sections
+    return n.join(",");
+}
+
+
+
+var calculatePrice = function () {
+    $("#js-price-desktop").val(desktopPrice[$("#js-slider-desktop").val()]);
+    $("#js-volume-desktop").html(desktopVolume[$("#js-slider-desktop").val()]);
+
+    $("#js-price-phone").val(phonePrice[$("#js-slider-phone").val()]);
+    $("#js-volume-phone").html(phoneVolume[$("#js-slider-phone").val()]);
+
+    $("#js-price-mobile").val(mobilePrice[$("#js-slider-mobile").val()]);
+    $("#js-volume-mobile").html(mobileVolume[$("#js-slider-mobile").val()]);
+    $("#js-volume-mobile-minutes").val(mobileAddedMin[$("#js-slider-mobile").val()]);
+    $("#js-volume-mobile-sms").val(mobileAddedSms[$("#js-slider-mobile").val()]);
+
+    $("#js-price-mobile-vol").val(mobilePriceVol[$("#js-slider-mobile-vol").val()]);
+    $("#js-volume-mobile-vol").html(mobileVolumeVol[$("#js-slider-mobile-vol").val()]);
+    // $("#js-volume-mobile-vol").html(mobileVolumeVol[$("#js-slider-mobile-vol").val()]);
+
+    // $(function() {
+    // var x = $("#js-slider-mobile-vol").slider();
+    // $("#js-slider-mobile").slide(function() {
+    //     x.slider('setValue', 2);
+    //     });
+    // });
+
+    $("#js-volume-mobile-sms-val").val(mobileVolumeVolSms[$("#js-slider-mobile-vol").val()]);
+    $("#js-volume-mobile-min-val").val(mobileVolumeVolMin[$("#js-slider-mobile-vol").val()]);
+
+    $("#js-price-dongle").val(donglePrice[$("#js-slider-dongle").val()]);
+    $("#js-volume-dongle").html(dongleVolume[$("#js-slider-dongle").val()]);
+
+    var desktop = parseFloat($("#js-price-desktop").val());
+    var phonecount = parseFloat($("#js-counted-phone").val());
+    var phone = parseFloat($("#js-price-phone").val());
+    var mobilecount = parseFloat($("#js-counted-mobile").val());
+    var mobile = parseFloat($("#js-price-mobile").val());
+    var mobilevol = parseFloat($("#js-price-mobile-vol").val());
+    var donglecount = parseFloat($("#js-counted-dongle").val());
+    var dongle = parseFloat($("#js-price-dongle").val());
+
+    if (desktop > 0 && phonecount > 0) {
+        var phonecountsum = (phonecount * 4.15) - 4.15;
+    }
+
+    else {
+        var phonecountsum = phonecount * 4.15;
+    }
+
+    var phonesum = ( phonecount * phone ) + phonecountsum;
+    var mobilesum = mobilecount * (mobile + mobilevol);
+    var donglesum = donglecount * dongle;
+    
+
+    var counted =  desktop + phonesum + mobilesum + donglesum;
+
+
+    var countedround = counted.toFixed(2);
+    var comnum = ReplaceNumberWithCommas(countedround);
+    $("#js-sum").html(comnum);
+
+
+    var summedsms = parseFloat($("#js-volume-mobile-sms").val());
+    var summedsmsval = parseFloat($("#js-volume-mobile-sms-val").val());
+    var smssum = summedsms + summedsmsval;
+    $('#smssum').html(smssum);
+
+    var summedmin = parseFloat($("#js-volume-mobile-minutes").val());
+    var summedminval = parseFloat($("#js-volume-mobile-min-val").val());
+    var minsum = summedmin + summedminval;
+    $('#minsum').html(minsum);
+
+
+// Fill the form //
+
+    $("#js-volume-desktopStripped").val($("#js-volume-desktop").text());
+
+    $("#js-volume-phoneStripped").val($("#js-volume-phone").text());
+    $("#js-counted-phoneVol").val(phonecount);
+    $("#js-counted-phonePrice").val((phonecountsum).toFixed(2));
+    $("#js-counted-phoneSum").val((phonesum).toFixed(2));
+
+    $("#js-counted-mobileCount").val(mobilecount);
+    $('#js-mobilesum').val((mobilesum).toFixed(2));
+
+    $("#js-volume-dongleStripped").val($("#js-volume-dongle").text());
+    $("#js-counted-dongleCount").val(donglecount);
+    $('#js-donglesum').val((donglesum).toFixed(2));
+
+};
+
+var count = 0;
+
+    var addOneDongle = function () {
+            count++;
+        $("#js-dongleamount").html(count);
+        calculatePrice();
+    };
+
+$(document).ready( function() {
+
+    $("#js-slider-desktop").slider()
+    .on('slide', function(ev){
+        calculatePrice();
+    });
+
+    $("#js-slider-phone").slider()
+    .on('slide', function(ev){
+        calculatePrice();
+    });
+
+    
+    $("#js-slider-mobile").slider()
+    .on('slide', function(ev){
+        calculatePrice();
+        //     if ($("#js-slider-mobile").val() > 0) {
+        //     $("#js-volume-mobile-vol").html(mobileAddedMin[$("#js-slider-mobile").val()]);
+        // }
+         // $("#js-volume-mobile-vol").html(mobileAddedMin[$("#js-slider-mobile").val()]);
+         
+    });
+
+    $("#js-slider-mobile-vol").slider()
+    .on('slide', function(ev){
+        calculatePrice();
+    });
+
+    $("#js-slider-dongle").slider()
+    .on('slide', function(ev){
+        calculatePrice();
+    });
+
+
+
+    $(".js-onedown").click(function() {
+        if (count > 0)
+            count--;
+        $(this).next().val(count);
+        $(this).next().next().html(count);
+
+        calculatePrice();
+    });
+
+    $(".js-oneup").click(function() {
+        count++;
+        $(this).prev().prev().val(count);
+        $(this).prev().html(count);
+
+        calculatePrice();
+    });
+
+
+        $("#js-price-desktop").val("0");
+
+        $("#js-counted-phone").val("0");
+        $("#js-price-phone").val("0");
+
+        $("#js-counted-mobile").val("0");
+        $("#js-price-mobile").val("0");
+        $("#js-price-mobile-vol").val("0");
+
+        $("#js-volume-mobile-minutes").val("0");
+        $("#js-volume-mobile-min-val").val("0");
+        $("#js-volume-mobile-sms").val("0");
+        $("#js-volume-mobile-sms-val").val("0");
+
+        $("#js-counted-dongle").val("0");
+        $("#js-price-dongle").val("0");
+
+        calculatePrice();
+
+    });
+
+        // Form validation
+$("#js-confirm-calc").click(function(){
+    $("#calcwrapper").slideUp('slow');
+    $("#contactwrapper").show();
+});
+
+$("#js-back").click(function(){
+    $("#contactwrapper").hide();
+    $("#calcwrapper").show();
+})
+
+function required() {
+    var empt = $(".form-group :input").val();
+    if (empt =="") {
+        $(".form-group").addClass("has-error");
+        $(".alert-danger").slideDown("fast");
+        return false;
+    }
+    else {
+        // $(".alert-danger").slideUp("fast");
+        $(".form-group").removeClass("has-error");
+        $("#contactwrapper").slideUp('slow');
+        $("#calcwrapper").hide();
+        $("#thankswrapper").show();
+        return true;
+    }
+}
+
+
